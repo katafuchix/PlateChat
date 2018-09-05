495fd6b4183b1c1c581e2aa5a5003c944f094619
katafuchix <katafuchi@deskplate.net>
2018-09-05 22:25:46 +0900

    ソース整理

--- PlateChat/Extensions/UIImageView+Extended.swift
+++ PlateChat/Extensions/UIImageView+Extended.swift
@@ -1,27 +1,26 @@
 //
-//  UIImageView+WebImage.swift
-//  matching
+//  UIImageView+Extended.swift
+//  PlateChat
 //
-//  Created by xxxAIRINxxx on 2017/02/08.
-//  Copyright © 2017 NOWALL, Inc. All rights reserved.
+//  Created by cano on 2018/09/05.
+//  Copyright © 2018年 deskplate. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import SDWebImage
 
 extension UIImageView {
-    
+
     func loadWebImage(_ urlString: String?, _ placeholderImage: UIImage? = nil, completion: ((UIImage?) -> Void)? = nil) {
         self.loadWebImageWithAnimation(urlString, placeholderImage) { image in
             completion?(image)
         }
     }
-    
+
     func loadWebImageWithAnimation(_ urlString: String?, _ placeholderImage: UIImage? = nil, completion: ((UIImage?) -> Void)?) {
         self.loadWebImageWithURL(urlString, placeholderImage) { [weak self]  image, error, cacheType, url in
             guard let _self = self else { return }
-            
+
             if cacheType == .none {
                 UIView.transition(with: _self,
                                   duration: 0.2,
@@ -42,7 +41,7 @@ extension UIImageView {
             if let _placeholderImage = placeholderImage { self.image = _placeholderImage }
             return
         }
-        
+
         self.sd_setImage(with: URL(string: _urlString),
                          placeholderImage: placeholderImage,
                          options: .retryFailed,
